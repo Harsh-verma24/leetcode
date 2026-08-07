be24c8de8f46bf1501129c29807d42ce9ee4dd88
Harsh verma <iamharshvrma@gmail.com>
2026-08-08 01:33:05 +0530

    Add 242. Valid Anagram

--- leetcode/Hashing/0242-valid-anagram/solution.java
+++ leetcode/Hashing/0242-valid-anagram/solution.java
@@ -1,14 +1,26 @@
-class Solution {
-    public boolean isAnagram(String s, String t) {
+class Solution {
+    public boolean isAnagram(String s, String t) {
 
-        if(s.lenght != t.length){
-            return false;
-        }
+        if(s.length() != t.length()){
+            return false;
+        }
 
-        HashMap<Integer,Integer> countS = new HashMap<>();
-        HashMap<Integer,Integer> countT = new HashMap<>();
+        HashMap<Character,Integer> countS = new HashMap<>();
+        HashMap<Character,Integer> countT = new HashMap<>();
 
-        for(char c : s.toCharArray()){
-            countS.put(c , countS.getOrDefault(c,0)+1);
-        }
-         for(char c : t.toCharArray()){
+        for(char c : s.toCharArray()){
+            countS.put(c , countS.getOrDefault(c,0)+1);
+        }
+         for(char c : t.toCharArray()){
+            countT.put(c , countT.getOrDefault(c,0)+1);
+        }
+        
+        for(char c : s.toCharArray()){
+            if(!countS.get(c).equals(countT.get(c))){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
