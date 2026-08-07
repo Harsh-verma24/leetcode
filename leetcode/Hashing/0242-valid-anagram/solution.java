5a5351be830116fcc0c8f332864440b64b1fd74d
Harsh verma <iamharshvrma@gmail.com>
2026-08-08 00:53:28 +0530

    Add 242. Valid Anagram

--- leetcode/Hashing/0242-valid-anagram/solution.java
+++ leetcode/Hashing/0242-valid-anagram/solution.java
@@ -1,5 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
-    }
-}
+
+        if(s.lenght != t.length){
+            return false;
+        }
+
+        HashMap<Integer,Integer> countS = new HashMap<>();
+        HashMap<Integer,Integer> countT = new HashMap<>();
+
+        for(char c : s.toCharArray()){
+            countS.put(c , countS.getOrDefault(c,0)+1);
+        }
+         for(char c : t.toCharArray()){
