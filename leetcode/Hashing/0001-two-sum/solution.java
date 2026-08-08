dace3afc1a20feccbd22f816fceaf70bb787cb2a
Harsh verma <iamharshvrma@gmail.com>
2026-08-08 14:19:33 +0530

    Add 1. Two Sum

--- leetcode/Hashing/0001-two-sum/solution.java
+++ leetcode/Hashing/0001-two-sum/solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        for(int i =0 ; i<n ;i++){
-            for(int j=i+1; j<n ; j++){
-                if (nums[i]+nums[j] == target){
-                    return new int[]{i,j};
-                }
+        HashMap<Integer,Integer> index = new HashMap<>();
+        for(int i = 0 ; i<nums.length ; i ++){
+            int diff = target - nums[i];
+            if(index.containsKey(diff)){
+                return new int[]{i,index.get(diff)};
             }
+            index.put(nums[i],i);
         }
 
         return new int[]{};
